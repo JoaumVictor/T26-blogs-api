791342ed1481557ee360d062c40ead9ecf23ceeb
Victor <joaumvictor.oficial@gmail.com>
2022-07-18 18:01:21 -0300

    feat: :sparkles: Adiciona as migrations / requisito 01

---
+++ src/database/migrations/20220718202423-create-post-categories.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: { model:'BlogPosts', key:'id' }
+      },
+      categoryId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: { model:'Categories', key:'id' }
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
